2a385bfbaea38a382ab3da8603830f8a294f9b25
Patrick Etcheverry <patrick.etcheverry@iutbayonne.univ-pau.fr>
2022-08-15 07:28:41 +0200

    Ajout des classes Individu et Voiture ainsi que du programme principal.

---
+++ Individu.h
@@ -0,0 +1,44 @@
+/*
+  Spécifie une classe Individu en relation réciproque avec sa Voiture
+  Auteur : Lopistéguy
+  Version : 0.1
+  Date : 12/08/2022
+*/
+
+#ifndef INDIVIDU_H
+#define INDIVIDU_H
+
+using namespace std;
+
+#include "Voiture.h" // Utilisée ci-dessous
+#include <iostream>
+
+class Voiture; /* Annonce une définition ultérieure de Voiture
+                   permet de faire référence aux membres de Voiture dans la classe Individu */
+
+class Individu
+{
+    /// ATTRIBUTS
+  public:
+    string nom; // Le nom de l'individu
+    string prenom; // Le prénom de l'individu
+    Voiture *maVoiture; // Lien avec la voitue de l'individu
+
+    /// MÉTHODES
+  public:
+    // Constructeur
+    Individu(string = "", string = "");
+    // destructeur
+    ~Individu();
+
+    // Méthodes usuelles : toString
+    string toString();        // Affiche l'identité de l'individu
+    string toStringAndLink(); // Affiche l'identité de l'individu et de sa voiture s'il en a une
+
+    // Méthodes spécifiques
+    void majMaVoiture(Voiture *);
+    void setMaVoiture(Voiture *);
+    void supprimerLien();
+};
+
+#endif // INDIVIDU_H
